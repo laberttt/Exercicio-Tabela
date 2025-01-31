89f78d0694cffb24369b90bfa0b76285e6c5a3a3
laberttt <labert.ribeiroo@gmail.com>
2025-01-31 18:46:29 -0300

    chamando funções no main

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 #José Victor Justino de Oliveira - 20240010840
 #Labert Paixão Ribeiro - 20240011365
+
+from table import *
+
+tabela = criarTabela()
+exibir_tabela(tabela)
+addLinha(tabela)
+delLinha(tabela)
+addColuna(tabela)
+delColuna(tabela)
+print(f"Soma: {sumTable(tabela)}")
+print(f"Média: {mediaTable(tabela)}")
