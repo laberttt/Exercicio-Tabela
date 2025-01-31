3c94c60f25a7c29b72d1caaa2686b023972d616a
laberttt <labert.ribeiroo@gmail.com>
2025-01-31 16:18:57 -0300

    adicionando primeiro requisito

--- main.py
+++ main.py
@@ -1,1 +1,2 @@
-#Labert Paixão Ribeiro
+#José Victor Justino de Oliveira - 20240010840
+#Labert Paixão Ribeiro - 20240011365
--- table.py
+++ table.py
@@ -1,1 +1,21 @@
-#Labert Paixão Ribeiro
+#José Victor Justino de Oliveira - 20240010840
+#Labert Paixão Ribeiro - 20240011365
+
+#Biblioteca apenas para visualizar a tabela no terminal (pip install pandas)
+import pandas as pd
+
+#Primeiro Requesito
+def criarTabela():
+    print("\n-=CRIANDO TABELA=-\n")   
+    tabela = {}
+    nColunas = int(input("Número de colunas: "))
+
+    for i in range (nColunas):
+        nomeColuna = input(f"Nome da coluna {i}: ")
+        
+        linha = input(f"Digite as linhas da coluna {i} separados por (;): ").split(';')
+
+        tabela[nomeColuna] = linha
+
+    return tabela
+
