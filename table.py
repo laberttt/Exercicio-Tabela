49cfe79f065166ef0a59e98099b33bd575166cc0
laberttt <labert.ribeiroo@gmail.com>
2025-01-31 18:44:35 -0300

    adicionando 4º, 5º, 6º, 7º e 8º requisito

--- table.py
+++ table.py
@@ -51,3 +51,77 @@ def delLinha(tabela):
     print("Removido com Sucesso!")
 
     return exibir_tabela(tabela)
+                
+#Quarto Requesito
+def addColuna(tabela):
+    print("\n-=ADICIONANDO COLUNA=-\n")  
+    coluna = input("Digite o nome da nova coluna: ").capitalize()
+
+    adicionar_coluna = input("Digite as linhas separados por (;): ").split(';')
+    
+    cont_linha  = list(tabela.values())
+
+    while len(adicionar_coluna) != len(cont_linha[0]):
+        print(f"Você esta adicionando mais linhas do que colunas, temos {len(cont_linha[0])}")
+        adicionar_coluna = input("Digite as linhas separados por (;): ").split(';')
+
+    tabela [coluna] = adicionar_coluna
+    
+    return exibir_tabela(tabela)
+        
+
+#Quinto Requesito
+def delColuna(tabela):
+    print("\n-=REMOVENDO COLUNA=-\n")
+    print(f"Colunas Válidas: {[x for x in tabela.keys()]}")
+    coluna_remove = input ("Digite o nome da coluna para remover: ").capitalize()
+    print("\n")
+
+    while coluna_remove not in tabela.keys():
+        print ("Nome de Coluna Inválido! Tente Novamente")
+        print(f"Colunas Válidas: {[x for x in tabela.keys()]}")
+        coluna_remove = input ("Digite o nome da coluna para remover: ")
+
+    tabela.pop(coluna_remove)
+
+    return exibir_tabela(tabela)
+        
+#Sexto Requesito
+def sumTable(tabela):
+    print("\n-=SOMANDO OS ITENS DA TABELA=-\n")  
+    soma = 0
+
+    for i in tabela.values():
+        for j in i:
+            if j.isdigit():
+                soma+=int(j)
+            else:
+                pass
+    if soma==0:
+        return "Não possui Inteiros\n"
+    else:
+        return soma
+
+#Sétimo Requesito
+def mediaTable(tabela):
+    print("\n-=MÉDIA DOS ITEMS DA TABELA=-\n")  
+    soma = 0
+    cont = 0
+
+    for i in tabela.values():
+        for j in i:
+            if j.isdigit():
+                soma += int(j)
+                cont += 1
+            else:
+                pass
+    if cont == 0:
+        return 'Não possui Inteiros\n'
+    else:
+        return (soma/cont)
+
+#Oitavo Requesito
+def exibir_tabela(tabela):
+    print("\n-=EXIBINDO A TABELA=-\n")  
+    print(pd.DataFrame(tabela))
+
